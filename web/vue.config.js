6ad9f7b7e504c9e4730741c298d19dba0a4d20ba
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-03-13 18:25:40 -0400

    Add sentry webpack plugin

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,8 +1,16 @@
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+
 process.env.VUE_APP_VERSION = process.env.COMMIT_REF;
 
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
     devtool: 'source-map',
+    plugins: [
+      new SentryWebpackPlugin({
+        include: '.',
+        ignore: ['node_modules', 'vue.config.js', 'webpack.config.js'],
+      }),
+    ],
   },
 };
